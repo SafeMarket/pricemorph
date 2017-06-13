b16d1832be2b9df52ff38045d0159139bcebb88a
Aakil Fernandes <aakilfernandes@gmail.com>
2017-06-13 19:38:12 -0400

    use PricemorphZone

--- index.js
+++ index.js
@@ -1,76 +1,24 @@
-const CrossConverter = require('cross-converter')
 const arguguard = require('arguguard')
+const _Pricemorph = require('./lib/Pricemorph')
 
-function Pricemorph(rate, numerator) {
-  arguguard('Pricemorph', ['Amorph', 'string'], arguments)
-  this.rate = rate
+function PricemorphZone(numerator) {
+  arguguard('PricemorphZone', ['string'], arguments)
   this.numerator = numerator
-}
+  this.rates = {}
 
-Pricemorph.crossConverter = new CrossConverter()
-
-Pricemorph.loadPricemorph = function loadPricemorph(pricemorph, denominator) {
-  arguguard('Pricemorph.loadPricemorph', ['Pricemorph', 'string'], arguments)
-  const Amorph = pricemorph.rate.constructor
-  Pricemorph.crossConverter.addConverter(pricemorph.numerator, denominator, (amorph) => {
-    const rateBignumber = amorph.to('bignumber').div(pricemorph.rate.to('bignumber'))
-    return new Amorph(rateBignumber, 'bignumber')
-  })
-  Pricemorph.crossConverter.addConverter(denominator, pricemorph.numerator, (amorph) => {
-    const rateBignumber = amorph.truth.times(pricemorph.rate.to('bignumber'))
-    return new Amorph(rateBignumber, 'bignumber')
-  })
-}
-
-Pricemorph.prototype.to = function to(numerator) {
-  arguguard('pricemorph.to', ['string'], arguments)
-  const Amorph = this.rate.constructor
-  if (this.rate.to('bignumber').equals(0)) {
-    return new Amorph(0, 'number');
-  }
-  if (this.numerator === numerator) {
-    return new Amorph(this.rate.to('bignumber'), 'bignumber');
+  const pricemorphZone = this
+  this.Pricemorph = function Pricemorph(rate, numerator) {
+    arguguard('Pricemorph', ['Amorph', 'string'], arguments)
+    return new _Pricemorph(pricemorphZone, rate, numerator)
   }
 
-  const rateAmorph = new Amorph(this.rate.to('bignumber'), 'bignumber')
-  return Pricemorph.crossConverter.convert(rateAmorph, this.numerator, numerator)
+  this.Pricemorph.prototype  = Object.create(_Pricemorph.prototype)
+  this.Pricemorph.constructor = this.Pricemorph
 }
 
-Pricemorph.prototype.plus = function plus(pricemorph) {
-  arguguard('pricemorph.plus', ['Pricemorph'], arguments)
-  const price = this.rate.as('bignumber', (bignumber) => {
-    return bignumber.plus(pricemorph.to(this.numerator).to('bignumber'))
-  })
-  return new Pricemorph(price, this.numerator)
+PricemorphZone.prototype.setRate = function setRate(numerator, Amorph) {
+  arguguard('pricemorphZone.setRate', ['string', 'Amorph'], arguments)
+  this.rates[numerator] = Amorph
 }
 
-Pricemorph.prototype.minus = function minus(pricemorph) {
-  arguguard('pricemorph.minus', ['Pricemorph'], arguments)
-  const price = this.rate.as('bignumber', (bignumber) => {
-    return bignumber.minus(pricemorph.to(this.numerator).to('bignumber'))
-  })
-  return new Pricemorph(price, this.numerator)
-}
-
-Pricemorph.prototype.times = function times(multiplier) {
-  arguguard('pricemorph.times', ['Amorph'], arguments)
-  const price = this.rate.as('bignumber', (bignumber) => {
-    return bignumber.times(multiplier.to('bignumber'))
-  })
-  return new Pricemorph(price, this.numerator)
-}
-
-Pricemorph.prototype.div = function div(multiplier) {
-  arguguard('pricemorph.div', ['Amorph'], arguments)
-  const price = this.rate.as('bignumber', (bignumber) => {
-    return bignumber.div(multiplier.to('bignumber'))
-  })
-  return new Pricemorph(price, this.numerator)
-}
-
-Pricemorph.prototype.toLabel = function toLabel(numerator, fixed) {
-  arguguard('pricemorph.toLabel', ['string', 'number'], arguments)
-  return `${this.to(numerator).to('bignumber').toFixed(fixed)} ${numerator}`
-}
-
-module.exports = Pricemorph
+module.exports = PricemorphZone
--- test.js
+++ test.js
@@ -1,9 +1,8 @@
 const Amorph = require('amorph')
-const Pricemorph = require('./')
+const PricemorphZone = require('./')
 const chai = require('chai')
 const chaiAmorph = require('chai-amorph')
 const bignumberPlugin = require('amorph-bignumber')
-const NoPathError = require('cross-converter/errors/NoPath')
 
 chai.use(chaiAmorph)
 chai.should()
@@ -11,10 +10,11 @@ chai.should()
 Amorph.loadPlugin(bignumberPlugin)
 
 const expect = chai.expect
-
 describe('Pricemorph', () => {
 
   let BTCPricemorphBTC
+  let pricemorphZone = new PricemorphZone('BTC')
+  const Pricemorph = pricemorphZone.Pricemorph
 
   it('new Pricemorph({}, BTC) should throw Error', () => {
     (() => {
@@ -28,40 +28,28 @@ describe('Pricemorph', () => {
     }).should.throw(Error)
   })
 
-  it('should instantiate', () => {
-    BTCPricemorphBTC = new Pricemorph(new Amorph(1, 'number'), 'BTC')
-    BTCPricemorphUSD = new Pricemorph(new Amorph(100, 'number'), 'USD')
-    //USC = US Cents
-    BTCPricemorphUSC = new Pricemorph(new Amorph(100*100, 'number'), 'USC')
-    //USN = US Nickels
-    BTCPricemorphUSN = new Pricemorph(new Amorph(100*20, 'number'), 'USN')
-    BTCPricemorphEUR = new Pricemorph(new Amorph(200, 'number'), 'EUR')
-  })
-
-
   it('pricemorph.to({}) should throw Error', () => {
     (() => {
       BTCPricemorphBTC.to({})
     }).should.throw(Error)
   })
 
-  it('pricemorph.loadPricemorph({}, BTC) should throw Error', () => {
+  it('pricemorphZone.setRate({}, BTC) should throw Error', () => {
     (() => {
-      Pricemorph.loadPricemorph({}, 'BTC')
+      pricemorphZone.setRate({}, 'BTC')
     }).should.throw(Error)
   })
 
-  it('pricemorph.loadPricemorph(pricemoprh, {}) should throw Error', () => {
+  it('pricemorphZone.setRate(amorph, {}) should throw Error', () => {
     (() => {
-      Pricemorph.loadPricemorph(BTCPricemorphBTC, {})
+      pricemorphZone.setRate(new Amorph(1, 'number'), {})
     }).should.throw(Error)
   })
 
-  it('should load pricemorphs', () => {
-    Pricemorph.loadPricemorph(BTCPricemorphBTC, 'BTC')
-    Pricemorph.loadPricemorph(BTCPricemorphUSD, 'BTC')
-    Pricemorph.loadPricemorph(BTCPricemorphUSC, 'BTC')
-    Pricemorph.loadPricemorph(BTCPricemorphEUR, 'BTC')
+  it('should set rates', () => {
+    pricemorphZone.setRate('USD', new Amorph(100, 'number'))
+    pricemorphZone.setRate('USC', new Amorph(100 * 100, 'number'))
+    pricemorphZone.setRate('EUR', new Amorph(200, 'number'))
   })
 
   it('should simple convert', () => {
@@ -72,23 +60,14 @@ describe('Pricemorph', () => {
     productPricemorph.to('EUR').should.amorphTo('number').equal(200 * 2)
   })
 
-  it('should throw NoPathError', () => {
-    (() => {
-      const productPricemorph = new Pricemorph(new Amorph(1, 'number'), 'EUR')
-      productPricemorph.to('USD').should.amorphTo('number').equal(.5)
-    }).should.throw(NoPathError)
-  })
-
   it('should cross convert (after adding paths)', () => {
     const productPricemorph = new Pricemorph(new Amorph(1, 'number'), 'EUR')
-    Pricemorph.crossConverter.addPath(['EUR', 'BTC', 'USD'])
-    Pricemorph.crossConverter.addPath(['EUR', 'BTC', 'USC'])
     productPricemorph.to('USD').should.amorphTo('number').equal(.5)
     productPricemorph.to('USC').should.amorphTo('number').equal(50)
   })
 
-  it('should load USN', () => {
-    Pricemorph.loadPricemorph(BTCPricemorphUSN, 'BTC')
+  it('should set rate for USN', () => {
+    pricemorphZone.setRate('USN', new Amorph(100 * 20, 'number'))
   })
 
   it('should simple convert', () => {
@@ -102,9 +81,6 @@ describe('Pricemorph', () => {
 
   it('should cross convert (after adding paths)', () => {
     const productPricemorph = new Pricemorph(new Amorph(1, 'number'), 'EUR')
-    Pricemorph.crossConverter.addPath(['EUR', 'BTC', 'USD'])
-    Pricemorph.crossConverter.addPath(['EUR', 'BTC', 'USC'])
-    Pricemorph.crossConverter.addPath(['EUR', 'BTC', 'USN'])
     productPricemorph.to('USD').should.amorphTo('number').equal(.5)
     productPricemorph.to('USC').should.amorphTo('number').equal(50)
     productPricemorph.to('USN').should.amorphTo('number').equal(10)
@@ -135,7 +111,6 @@ describe('Pricemorph', () => {
   })
 
   it('should toLabel', () => {
-    Pricemorph.crossConverter.addPath(['USD', 'BTC', 'EUR'])
     const pricemorph1 = new Pricemorph(new Amorph(1, 'number'), 'USD')
     pricemorph1.toLabel('USD', 0).should.equal('1 USD')
     pricemorph1.toLabel('EUR', 0).should.equal('2 EUR')
